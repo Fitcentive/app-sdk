eb7277bd0347dde0a2b9cd1cb121e8fb484e06b6
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2022-07-05 15:57:48 -0400

    - Add anorm op

--- src/main/scala/io/fitcentive/sdk/utils/AnormOps.scala
+++ src/main/scala/io/fitcentive/sdk/utils/AnormOps.scala
@@ -2,6 +2,8 @@ package io.fitcentive.sdk.utils
 
 import anorm.NamedParameter
 
+import java.util.UUID
+
 trait AnormOps extends StringOps {
 
   /**
@@ -15,5 +17,12 @@ trait AnormOps extends StringOps {
         s"$snakeName = case when {${p.name}} is null then u.$snakeName else {${p.name}} end"
       }
       .mkString(", ")
+
+  /**
+    * Required SQL statement upto opening parenthesis
+    * For example, select * from table_name where id in (
+    */
+  def transformUuidsToSql(uuids: Seq[UUID], sql: String): String =
+    uuids.foldLeft(sql)((acc, id) => acc + s"'$id'::uuid,").dropRight(1) + ") ;"
 
 }

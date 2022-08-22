e1ebf6c3c7bb14b92cc40e8bdc364b14eee6cee0
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2022-08-22 19:48:45 -0400

    - Make firstname/lastname optional to support Apple sign in

--- src/main/scala/io/fitcentive/sdk/play/domain/AuthorizedUser.scala
+++ src/main/scala/io/fitcentive/sdk/play/domain/AuthorizedUser.scala
@@ -5,7 +5,13 @@ import io.circe.{Decoder, HCursor}
 
 import java.util.UUID
 
-case class AuthorizedUser(username: String, userId: UUID, firstName: String, lastName: String, email: String)
+case class AuthorizedUser(
+  username: String,
+  userId: UUID,
+  firstName: Option[String],
+  lastName: Option[String],
+  email: String
+)
 
 object AuthorizedUser {
   implicit val decoder: Decoder[AuthorizedUser] = new Decoder[AuthorizedUser] {
@@ -13,8 +19,8 @@ object AuthorizedUser {
       for {
         username <- c.downField("preferred_username").as[String]
         userId <- c.downField("user_id").as[UUID]
-        firstName <- c.downField("given_name").as[String]
-        lastName <- c.downField("family_name").as[String]
+        firstName <- c.downField("given_name").as[Option[String]]
+        lastName <- c.downField("family_name").as[Option[String]]
         email <- c.downField("email").as[String]
       } yield AuthorizedUser(username, userId, firstName, lastName, email)
   }
--- src/main/scala/io/fitcentive/sdk/play/domain/AuthorizedUserWithoutId.scala
+++ src/main/scala/io/fitcentive/sdk/play/domain/AuthorizedUserWithoutId.scala
@@ -3,15 +3,15 @@ package io.fitcentive.sdk.play.domain
 import io.circe.Decoder.Result
 import io.circe.{Decoder, HCursor}
 
-case class AuthorizedUserWithoutId(username: String, firstName: String, lastName: String, email: String)
+case class AuthorizedUserWithoutId(username: String, firstName: Option[String], lastName: Option[String], email: String)
 
 object AuthorizedUserWithoutId {
   implicit val decoder: Decoder[AuthorizedUserWithoutId] = new Decoder[AuthorizedUserWithoutId] {
     final def apply(c: HCursor): Result[AuthorizedUserWithoutId] =
       for {
         username <- c.downField("preferred_username").as[String]
-        firstName <- c.downField("given_name").as[String]
-        lastName <- c.downField("family_name").as[String]
+        firstName <- c.downField("given_name").as[Option[String]]
+        lastName <- c.downField("family_name").as[Option[String]]
         email <- c.downField("email").as[String]
       } yield AuthorizedUserWithoutId(username, firstName, lastName, email)
   }
